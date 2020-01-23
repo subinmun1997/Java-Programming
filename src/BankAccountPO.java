324ac912a4760227caaed4f00d9e412897a00872
subinmun1997 <52366841+subinmun1997@users.noreply.github.com>
2020-01-23 14:02:13 +0900

    Java programming

---
+++ src/BankAccountPO.java
@@ -0,0 +1,23 @@
+import java.io.*;
+class BankAccountPO {
+	static int balance=0;
+	
+	public static void main(String[] args) {
+		deposit(10000);
+		checkMyBalance();
+		withdraw(3000);
+		checkMyBalance();
+	}
+	public static int deposit(int amount) {
+		balance+=amount;
+		return balance;
+	}
+	public static int withdraw(int amount){
+		balance-=amount;
+		return balance;
+	}
+	public static int checkMyBalance() {
+		System.out.println("ภÜพื : "+balance);
+		return balance;
+	}
+}
