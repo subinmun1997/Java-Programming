bb130ddbd97be5a04ecb346d6e6239ba3da3044d
subinmun1997 <52366841+subinmun1997@users.noreply.github.com>
2020-02-05 16:00:56 +0900

    Java programming

---
+++ src/Wrapping2.java
@@ -0,0 +1,30 @@
+class Box{
+	public void Wrap() {
+		System.out.println("Simple Wrapping");
+	}
+}
+class PaperBox extends Box{
+	public void Wrap() {
+		System.out.println("Paper Wrapping");
+	}
+}
+class GoldPaperBox extends PaperBox{
+	public void Wrap() {
+		System.out.println("Gold Wrapping");
+	}
+}
+
+class Wrapping2 {
+	public static void main(String[] args) {
+		Box box1 = new Box();
+		PaperBox box2 = new PaperBox();
+		GoldPaperBox box3 = new GoldPaperBox();
+		
+		wrapBox(box1);
+		wrapBox(box2);
+		wrapBox(box3);
+	}
+	public static void wrapBox(Box box) {
+		box.Wrap();
+	}
+}
--- src/YummyCakeSuper.java
+++ src/YummyCakeSuper.java
@@ -3,19 +3,23 @@ class Cake{
 		System.out.println("Yummy Cake");
 	}
 }
+
 class CheeseCake extends Cake{
 	public void yummy() {
+		super.yummy();
 		System.out.println("Yummy CheeseCake");
+	}
+	public void tasty() {
+		super.yummy();
+		System.out.println("Yummy Tasty Cake");
 	}
 }
 
-class YummyCakeOverriding {
+class YummyCakeSuper { 
 	public static void main(String[] args) {
-		Cake c1 = new CheeseCake();
-		CheeseCake c2 = new CheeseCake();
-		
+		CheeseCake c1 = new CheeseCake();
 		c1.yummy();
-		c2.yummy();
-		
+		c1.tasty();
 	}
+
 }
