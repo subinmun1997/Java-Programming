140720090c1730cc3d8add61145aeadae14d8d86
subinmun1997 <52366841+subinmun1997@users.noreply.github.com>
2020-02-06 20:23:55 +0900

    Java programming

---
+++ src/PrinterDriver3.java
@@ -0,0 +1,29 @@
+interface Printable{
+	void print(String doc);
+}
+
+interface ColorPrintable extends Printable{
+	void printCMYK(String doc);
+}
+
+class Prn909Drv implements ColorPrintable{
+	@Override
+	public void print(String doc) {
+		System.out.println("From MD-909 black&white ver");
+		System.out.println(doc);
+	}
+	public void printCMYK(String doc) {
+		System.out.println("From MD-909 CMYK ver");
+		System.out.println(doc);
+	}
+}
+
+class PrinterDriver3 {
+	public static void main(String[] args) {
+		String myDoc = "This is a report about...";
+		ColorPrintable prn = new Prn909Drv();
+		prn.print(myDoc);
+		System.out.println();
+		prn.printCMYK(myDoc);
+	}
+}
