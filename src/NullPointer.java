e0224e52e420d5acccb9c75def90480fc65430d7
subinmun1997 <52366841+subinmun1997@users.noreply.github.com>
2020-02-07 16:58:07 +0900

    Java programming

---
+++ src/NullPointer.java
@@ -0,0 +1,7 @@
+class NullPointer {
+	public static void main(String[] args) {
+		String str = null;
+		System.out.println(str);
+		int len = str.length();
+	}
+}
---
+++ src/SimplePrinter.java
@@ -0,0 +1,20 @@
+interface Printable{
+	static void printLine(String str) {
+		System.out.println(str);
+	}
+	default void print(String doc) {
+		printLine(doc);
+	}
+}
+
+class Printer implements Printable{}
+
+class SimplePrinter {
+	public static void main(String[] args) {
+		String myDoc = "This is report about...";
+		Printable prn = new Printer();
+		prn.print(myDoc);
+		
+		Printable.printLine("end of line");
+	}
+}
