7c8d75e1607ef045e952fc41262553302602372f
subinmun1997 <52366841+subinmun1997@users.noreply.github.com>
2020-02-16 16:26:53 +0900

    Java programming

---
+++ src/ArrayObjNameLength.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Person implements Comparable{
+	private String name;
+	private int age;
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	@Override
+	public int compareTo(Object o) {
+		Person p = (Person)o;
+		return this.name.length() - p.name.length();
+		
+	}
+	@Override
+	public String toString() {
+		return name + ":" + age;
+	}
+}
+
+class ArrayObjNameLength {
+	public static void main(String[] args) { 
+		Person[] ar = new Person[3];
+		ar[0] = new Person("LeeLee", 29);
+		ar[1] = new Person("GooGo", 15);
+		ar[2] = new Person("Soo", 37);
+		
+		Arrays.sort(ar);
+		for(Person p : ar)
+			System.out.println(p);
+	}
+}
