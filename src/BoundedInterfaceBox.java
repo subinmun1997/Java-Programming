ce7c64bf0013a32a309c7857e96c7e6274b9c7f4
subinmun1997 <52366841+subinmun1997@users.noreply.github.com>
2020-02-19 18:38:14 +0900

    Java programming

---
+++ src/BoundedInterfaceBox.java
@@ -0,0 +1,35 @@
+interface Eatable{
+	public String eat();
+}
+
+class Apple implements Eatable{
+	public String toStinrg() {
+		return "I am an apple.";
+	}
+	@Override
+	public String eat() {
+		return "It tastes so good!";
+	}
+}
+
+class Box<T extends Eatable>{
+	T ob;
+	
+	public void set(T o) {
+		ob = o;
+	}
+	public  T get() {
+		System.out.println(ob.eat());
+		return ob;
+	}
+}
+
+class BoundedInterfaceBox {
+	public static void main(String[] args) {
+		Box<Apple> box = new Box<>();
+		box.set(new Apple());
+		
+		Apple ap = box.get();
+		System.out.println(ap);
+	}
+}
