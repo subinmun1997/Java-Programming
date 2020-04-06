196c3b05e4afb6a572d68620e0782ac5bba99eb8
subinmun1997 <52366841+subinmun1997@users.noreply.github.com>
2020-04-07 02:51:42 +0900

    Java programming

---
+++ src/Lambda2.java
@@ -0,0 +1,14 @@
+interface Printable{
+	void print(String s);
+}
+
+class Lambda2 {
+	public static void main(String[] args) {
+		Printable prn = new Printable() {
+			public void print(String s) {
+				System.out.println(s);
+			}
+		};
+		prn.print("what is Lambda");
+	}
+}
